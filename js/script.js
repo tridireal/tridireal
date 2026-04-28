214edb078d43ec861e8957dad8c1014d602db4ac
tridireal <astrabreezy@gmail.com>
2026-04-29 00:21:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger'],
+    strings: ['Developer', 'Graphic Designer',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
